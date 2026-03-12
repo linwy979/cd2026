ddd3965dfd16cced584c513c87b88a91f4eb1b83
lin979 <86038968+linwy979@users.noreply.github.com>
2026-03-12 16:44:24 +0800

    Update main.c
    Refactor main function to improve character counting with a fast lookup array and ensure memory is freed before program termination.

--- hw0/main.c
+++ hw0/main.c
@@ -2,7 +2,7 @@
 #include <stdlib.h>
 #include <string.h>
 
-// 作業要求的 Linked List 結構
+
 struct my_node {
     char data;
     int count;
@@ -12,7 +12,7 @@ struct my_node {
 typedef struct my_node Node;
 
 int main() {
-    // 讀取自己這個檔案
+
     FILE *fptr = fopen(__FILE__, "r");
     if (fptr == NULL) {
         printf("Error: Cannot open file\n");
@@ -22,26 +22,24 @@ int main() {
     Node *head = NULL;
     Node *tail = NULL;
     
-    // 加分題優化：建立一個 256 大小的指標陣列來記錄看過的字元
-    // 這樣找字元的時間複雜度就是 O(1)
     Node *fast_look[256] = {NULL};
 
     int ch_val;
     while ((ch_val = fgetc(fptr)) != EOF) {
         unsigned char idx = (unsigned char)ch_val;
 
-        // 如果這個字元已經在我的 List 裡了
+       
         if (fast_look[idx] != NULL) {
             fast_look[idx]->count++;
         } 
         else {
-            // 沒看過的字元，新開一個空間給它
+            
             Node *newNode = (Node *)malloc(sizeof(Node));
             newNode->data = (char)ch_val;
             newNode->count = 1;
             newNode->next = NULL;
 
-            // 把新節點接在 List 的最後面
+           
             if (head == NULL) {
                 head = newNode;
                 tail = newNode;
@@ -49,16 +47,14 @@ int main() {
                 tail->next = newNode;
                 tail = newNode;
             }
-            // 存入加速表，下次遇到同字元就不必重新跑迴圈找
+           
             fast_look[idx] = newNode;
         }
     }
     fclose(fptr);
-
-    // 開始印出結果 (依照出現順序)
+   
     Node *ptr = head;
     while (ptr != NULL) {
-        // 處理空白和換行的特殊印出方式
         if (ptr->data == '\n') printf("\\n : %d\n", ptr->count);
         else if (ptr->data == ' ') printf("' ' : %d\n", ptr->count);
         else if (ptr->data == '\t') printf("\\t : %d\n", ptr->count);
@@ -67,7 +63,7 @@ int main() {
         ptr = ptr->next;
     }
 
-    // 養成好習慣：程式結束前釋放記憶體
+   
     Node *curr = head;
     while (curr != NULL) {
         Node *temp_ptr = curr;
